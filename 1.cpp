703ef1d7295f9a8ff3c0bfc41464a21433fe0d18
Djodi Putra Ramadhan <64624940+jodirmdhn@users.noreply.github.com>
2020-05-18 01:10:18 +0800

    Add files via upload

---
+++ 1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout<<"NAMA 	: Djodi Putra R \n\n";
+	cout<<"NIM  	: F1B019044 \n\n";
+	cout<<"KELPMPOK : 10\n\n";
+int nim[5]= {8,9,0,4,7};
+	
+	cout << "Nim ke-1 adalah " << nim[0] << endl;
+	cout << "Nim ke-2 adalah " << nim[1] << endl;
+	cout << "Nim ke-3 adalah " << nim[2] << endl;
+	cout << "Nim ke-4 adalah " << nim[3] << endl;
+	cout << "Nim ke-5 adalah " << nim[4] << endl;	
+
+}
+
